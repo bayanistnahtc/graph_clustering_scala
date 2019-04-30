93d5305596eb32076208d1adc7b9f759fddbd53a
RufinaMay <rufa-gali@.mail.ru>
2019-04-30 10:22:12 +0300

    send message changed

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -20,26 +20,20 @@ object main {
   val vertexRDD: RDD[(VertexId, Long)] = vertex_file.map(line => line.split(" "))
       .map(line => (line(0).toLong, line(0).toLong ))
 
+
+
   val myGraph = Graph(vertexRDD, edgesRDD)
 
-
-
-  //TakeNeighbourLables
-    def SendMSG(edge: EdgeTriplet[Long, Long]): Iterator[(VertexId, Map[Long, Long])] = {
-        Iterator((edge.dstId, Map(edge.srcAttr -> 1L)))//dstAttr or srcAttr??????????????????????????????????????????
-    }
   //Count labels
     def MergeMSG(msg1:Map[Long, Long], msg2:Map[Long, Long]):Map[Long, Long]={
       val msg = msg1.toList ++ msg2.toList
-//      val reduced = msg.reduceByKey(_ + _)//didnt work with pregel
-//      reduced.collectAsMap().toMap
       msg.groupBy(_._1).collect{case e => e._1 -> e._2.map(_._2).sum }
     }
   //MostFrequentLabel
     def VProg(vId: VertexId, label: Long, msgs:Map[Long, Long] ): Long={
       //if there is no message, leave the same label
       var finalLabel = label
-      //if there is a message, then return label that occurs most
+      //if there is a message, then returgit add n label that occurs most
       if (!msgs.isEmpty){
         finalLabel = msgs.maxBy(_._2)_1
       }
@@ -50,10 +44,11 @@ object main {
     val g = Pregel(
       graph = myGraph,
       initialMsg = Map.empty[Long, Long], //first - label, second - 1
-      maxIterations = 10,
-      activeDirection = EdgeDirection.Either//Кому рассылать непонятно
+      maxIterations = 1,
+      activeDirection = EdgeDirection.Either//Когда рассылать непонятно
     )(
-      sendMsg = SendMSG,
+      sendMsg = (edge:EdgeTriplet[Long,Long]) =>   //distribute labels
+        Iterator((edge.dstId, Map(edge.srcAttr -> 1L))),
       mergeMsg = MergeMSG,
       vprog = VProg
     )
